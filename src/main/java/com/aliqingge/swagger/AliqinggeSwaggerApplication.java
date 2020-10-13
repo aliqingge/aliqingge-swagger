e46343632299da3c6d81d1c26051cb3efb347125
zhangzhongqing <zhangzhongqing@gogpay.cn>
2020-10-13 15:06:21 +0800

    提交代码

---
+++ src/main/java/com/aliqingge/swagger/AliqinggeSwaggerApplication.java
@@ -0,0 +1,16 @@
+package com.aliqingge.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class AliqinggeSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliqinggeSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aliqingge/swagger/AliqinggeSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliqingge.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliqinggeSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
